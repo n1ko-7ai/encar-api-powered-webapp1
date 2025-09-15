ce61913fa9a841498dd595032ca9b24a040c04a6
n1ko._lai <yugayne2008@gmail.com>
2025-09-15 19:05:52 +0900

    Test

--- main.py
+++ main.py
@@ -321,6 +321,31 @@ session = requests.Session()
 def log(msg):
     print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] {msg}", flush=True)
 
+def update_cookies_and_tokens():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True,
+                                    args=[
+                                        "--window-size=1920,1080",
+                                        "--disable-blink-features=AutomationControlled"
+                                    ])
+        context = browser.new_context(
+            viewport={'width': 1920, 'height': 1080},
+            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36"
+        )
+        page = context.new_page()
+
+        # Эмуляция движений мыши и задержек может помочь
+        page.goto("https://www.encar.com/dc/dc_carsearchlist.do?carType=kor")
+
+        cookies_list = context.cookies()
+        cookies_dict = {c['name']: c['value'] for c in cookies_list}
+        session.cookies.update(cookies_dict)
+
+        for cookie in cookies_list:
+            print(f"{cookie['name']} : {cookie['value']}")
+
+        browser.close()
+
 def rates_refresher():
     while True:
         time.sleep(REFRESH_INTERVAL)
@@ -448,6 +473,8 @@ if __name__ == "__main__":
 
     log("Все прокси были отключены.")
 
+
+    update_cookies_and_tokens()
     log("Обновления курса")
     rate = get_exchange_rates()
     threading.Thread(target=rates_refresher, daemon=True).start()
